af6b46a5c62c0a61dc6e780018cf7c1946e9c313
David Rose <David010@gmail.com>
2018-04-05 17:15:51 -0400

    fixed self. issues

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -29,12 +29,12 @@ class Controller(object):
     	ts  = 0.02 # sample time
     	self.vel_lpf = LowPassFilter(tau , ts)
 
- 		vehicle_mass   = vehicle_mass
-        fuel_capacity  = fuel_capacity
-        brake_deadband = brake_deadband
-        decel_limit    = decel_limit
-        accel_limit    = accel_limit
-        wheel_radius   = wheel_radius
+ 		self.vehicle_mass   = vehicle_mass
+        self.fuel_capacity  = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit    = decel_limit
+        self.accel_limit    = accel_limit
+        self.wheel_radius   = wheel_radiuss
 
         self.last_time = rospy.get_time()
 
