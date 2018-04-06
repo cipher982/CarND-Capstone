c88426231fea8dfbb193b5efd1e7816e83727f81
David Rose <David010@gmail.com>
2018-04-06 15:28:42 -0400

    maybe typo

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -95,7 +95,7 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
-    def get_closest_waypoint_idx(self):
+    def get_closest_waypoint_id(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
