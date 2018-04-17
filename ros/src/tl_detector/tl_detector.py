76e9dc560d924abdc0b432756c710a58559c09d9
David Rose <David010@gmail.com>
2018-04-17 15:15:38 -0400

    adding logwarn for calling traffic code py

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -38,6 +38,7 @@ class TLDetector(object):
         config_string = rospy.get_param("/traffic_light_config")
         self.config   = yaml.load(config_string)
 
+        rospy.logwarn("trying to publish traffic_waypoint!")
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge           = CvBridge()
