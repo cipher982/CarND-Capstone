831bedbed7d62e0fa2fbb63d319b359cf2c26597
David Rose <David010@gmail.com>
2018-04-19 14:29:13 -0400

    removing that annoying logwarn

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -81,8 +81,8 @@ class WaypointUpdater(object):
 
         if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
             # clear, keep going
-            rospy.logwarn("using normal waypoints! stopline_wp_idx:{0}, farthest_idx:{1}".\
-                format(self.stopline_wp_idx,farthest_idx))
+            #rospy.logwarn("using normal waypoints! stopline_wp_idx:{0}, farthest_idx:{1}".\
+            #    format(self.stopline_wp_idx,farthest_idx))
             lane.waypoints = base_waypoints
         else:
             # not clear, slow down
